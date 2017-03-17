cd226035ff80e5bda8f2dc7544607a9cab0caf23
Cle61 <prodhomme.clement@hotmail.fr>
2017-03-17 10:48:56 +0100

    add sidebar on the book show

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register Book do
   menu parent: "Bookshop"
   permit_params :name, :state
+
+  sidebar "User Details", only: :show do
+    attributes_table_for book.user do
+      row :email
+    end
+  end
 end

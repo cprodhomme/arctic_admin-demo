fb25082eda85533dcf4df2d22febbef522563df9
Cle61 <prodhomme.clement@hotmail.fr>
2017-06-25 21:18:25 +0200

    add page css

---
+++ app/admin/css.rb
@@ -0,0 +1,13 @@
+ActiveAdmin.register_page "CSS" do
+  content do
+    button 'small button', class: 'small button'
+    button 'button', class: 'button'
+    button 'large button', class: 'large button'
+
+    code do
+      div "<button class='small button'>"
+      div "<button class='button'>"
+      div "<button class='large button'>"
+    end
+  end
+end

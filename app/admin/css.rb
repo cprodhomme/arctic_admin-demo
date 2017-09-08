323029e97a1101eb3e4a65e96cab718ab01732c8
Cle61 <prodhomme.clement@hotmail.fr>
2017-09-08 23:31:34 +0200

    improve css page

--- app/admin/css.rb
+++ app/admin/css.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register_page "CSS" do
   content do
+    a '<i class="fa fa-github" aria-hidden="true"></i> arctic_admin'.html_safe, href: 'https://github.com/cle61/arctic_admin', target: '_blank', class: 'button link-github'
+    a '<i class="fa fa-github" aria-hidden="true"></i> arctic_admin-demo'.html_safe, href: 'https://github.com/cle61/arctic_admin-demo', target: '_blank', class: 'button link-github-demo'
+
     h2 'Buttons'
 
     button 'small button', class: 'small button'
@@ -10,6 +13,22 @@ ActiveAdmin.register_page "CSS" do
       div "<button class='small button'>"
       div "<button class='button'>"
       div "<button class='large button'>"
+    end
+
+    h2 'Tabs'
+
+    tabs do
+      tab :active do
+        table_for 'Lorem Ipsum' do
+          span 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse consectetur metus ligula, a suscipit orci congue non. Nulla facilisi. Curabitur tempor, massa ac rhoncus congue, leo sem placerat magna, at dapibus odio erat sit amet ex. Quisque placerat sit amet ex quis viverra. Morbi in suscipit turpis. Sed magna erat, mollis ac placerat et, mollis venenatis lectus. Morbi rutrum aliquam accumsan. Donec quis mi non ipsum suscipit condimentum a sit amet lectus.'
+        end
+      end
+
+      tab :inactive do
+        table_for 'more infos' do
+          span 'Vestibulum dictum non magna vitae sagittis. Etiam purus orci, facilisis ut diam id, consectetur tincidunt ligula. Nullam sagittis, libero et maximus hendrerit, nisl tortor condimentum ex, at imperdiet sem lectus id turpis. Mauris eu erat quis erat sollicitudin blandit a nec arcu. Vestibulum eget consectetur mi, et sagittis massa. In non elementum nunc, ut maximus leo. Vestibulum ut elementum nibh. Nam facilisis libero in turpis ultricies faucibus. Praesent vel viverra lorem. Donec elementum tincidunt metus nec convallis. Nullam pharetra viverra dolor sit amet porttitor.'
+        end
+      end
     end
   end
 end
